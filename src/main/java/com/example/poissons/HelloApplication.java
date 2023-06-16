9a00bde62b619e79dd2fdad79e3d19ce038b0530
Seriwan <seriwan-isik@hotmail.com>
2023-06-16 16:12:05 +0200

    add fish to window and set the size of the general window

--- src/main/java/com/example/poissons/HelloApplication.java
+++ src/main/java/com/example/poissons/HelloApplication.java
@@ -3,21 +3,35 @@ package com.example.poissons;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.geometry.Point2D;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     static Poisson[] banc;
 
-    static final int WIDTH = 800;
+    static final int WIDTH = 600;
     static final int HEIGHT = 600;
     public void start(Stage stage) throws IOException {
+
+        
         Stage premierStage = new Stage();
-        premierStage.setTitle("Première fenêtre");
+        
+
+
+
+
+        premierStage.setTitle("Banc de Poisson");
         BorderPane root = new BorderPane();
         Button btnhello = new Button("Hello");
         btnhello.setOnAction(event -> {
@@ -26,10 +40,18 @@ public class HelloApplication extends Application {
                 poisson.deplacer();
             }
         });
-        root.setCenter(btnhello);
-        Scene scene = new Scene(root, 300, 250);
+       // root.setCenter(btnhello);
+        Scene scene = new Scene(root, HEIGHT, WIDTH);
         premierStage.setScene(scene);
         premierStage.show();
+
+        for (Poisson poisson : banc) {
+            Circle corpsPoisson = new Circle(3, Color.ORANGE);
+            corpsPoisson.setCenterX(poisson.x);
+            corpsPoisson.setCenterY(poisson.y);
+            poisson.deplacer();
+            root.getChildren().add(corpsPoisson);
+        }
 
 
     }
