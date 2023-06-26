cba6b8d652ecf4389cf92a58bd0e7ac2eeb103ed
AdrienLD <adrien.laduca@icloud.com>
2023-06-26 22:27:56 +0200

    modifs

--- src/main/java/com/example/poissons/HelloApplication.java
+++ src/main/java/com/example/poissons/HelloApplication.java
@@ -18,9 +18,9 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     static Poisson[] banc;
-    static final int NOMBRE_POISSONS = 20;
-    static final int WIDTH = 600;
-    static final int HEIGHT = 600;
+    static final int NOMBRE_POISSONS = 200;
+    static final int WIDTH = 900;
+    static final int HEIGHT = 900;
     public void start(Stage stage) throws IOException {
         Stage premierStage = new Stage();
         premierStage.setTitle("Première fenêtre");
@@ -65,53 +65,7 @@ public class HelloApplication extends Application {
         corpsPoisson.setCenterX(poisson.x);
         corpsPoisson.setCenterY(poisson.y);
         root.getChildren().add(corpsPoisson);
-        double angleRadian;
+        root.getChildren().add(poisson.vuePoisson);
 
-        // Calcul des coordonnées du premier point du triangle (point A)
-        angleRadian = Math.toRadians(poisson.direction - poisson.angleVision / 2);
-        double xA = poisson.x + poisson.distanceVision * Math.cos(angleRadian);
-        double yA = poisson.y + poisson.distanceVision * Math.sin(angleRadian);
-
-        // Calcul des coordonnées du deuxième point du triangle (point B)
-        angleRadian = Math.toRadians(poisson.direction + poisson.angleVision / 2);
-        double xB = poisson.x + poisson.distanceVision * Math.cos(angleRadian);
-        double yB = poisson.y + poisson.distanceVision * Math.sin(angleRadian);
-
-        // Création du triangle en utilisant les coordonnées des points
-        Polygon triangle = new Polygon();
-        triangle.getPoints().addAll(
-                poisson.x, poisson.y,  // Coordonnées du poisson (point de base du triangle)
-                xA, yA,                // Coordonnées du point A
-                xB, yB                 // Coordonnées du point B
-        );
-
-        // Paramétrage du style du triangle
-        triangle.setFill(Color.GREEN);
-        triangle.setStroke(Color.TRANSPARENT);
-
-        for (Poisson autrespoissons : banc){
-            if (autrespoissons == poisson){
-                continue;
-            }
-            if (triangle.contains(autrespoissons.x, autrespoissons.y)){
-                double dx = autrespoissons.x - poisson.x;
-                double dy = autrespoissons.y - poisson.y;
-                double distance = Math.sqrt(dx * dx + dy * dy);
-                double angleAbsolu = Math.toDegrees(Math.atan2(dy, dx));
-                double angleRelatif = angleAbsolu - poisson.direction;
-                if (angleRelatif < -180) angleRelatif += 360;
-                if (angleRelatif > 180) angleRelatif -= 360;
-                if (angleRelatif > 0){
-                    poisson.direction += 0.5;
-                } else if (angleRelatif < 0) {
-                    poisson.direction -= 0.5;
-                }
-                triangle.setFill(Color.RED);
-            }
-        }
-
-
-        // Ajout du triangle au conteneur
-        root.getChildren().add(triangle);
     }
 }
--- src/main/java/com/example/poissons/Poisson.java
+++ src/main/java/com/example/poissons/Poisson.java
@@ -1,6 +1,9 @@
 package com.example.poissons;
 
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
 public class Poisson {
     public int vitesse;
     public double x;
@@ -8,7 +11,7 @@ public class Poisson {
     public int direction;
     public static double angleVision = 30;
     public static double distanceVision = 40;
-
+    public Polygon vuePoisson;
 
     public static void setDistanceVision(double distanceVision) {
         Poisson.distanceVision = distanceVision;
@@ -21,7 +24,29 @@ public class Poisson {
         this.x = x;
         this.y = y;
         this.direction = direction;
+
     }
+
+    public void creerVue(){
+        vuePoisson = new Polygon();
+        double angleRadian;
+        angleRadian = Math.toRadians(direction - angleVision / 2);
+        double xA = x + distanceVision * Math.cos(angleRadian);
+        double yA = y + distanceVision * Math.sin(angleRadian);
+
+        // Calcul des coordonnées du deuxième point du triangle (point B)
+        angleRadian = Math.toRadians(direction + angleVision / 2);
+        double xB = x + distanceVision * Math.cos(angleRadian);
+        double yB = y + distanceVision * Math.sin(angleRadian);
+        vuePoisson.getPoints().addAll(
+                x, y,  // Coordonnées du poisson (point de base du triangle)
+                xA, yA,                // Coordonnées du point A
+                xB, yB                 // Coordonnées du point B
+        );
+        vuePoisson.setFill(Color.BLUE);
+        vuePoisson.setStroke(Color.TRANSPARENT);
+    }
+
     public void deplacer() {
         if (x < 0 || x > HelloApplication.WIDTH) {
             direction = 180 - direction;
@@ -32,9 +57,37 @@ public class Poisson {
         if (direction < 0) {
             direction += 360;
         }
+        creerVue();
         double angleRadians = Math.toRadians(direction);
         x += (double) (vitesse * Math.cos(angleRadians));
         y += (double) (vitesse * Math.sin(angleRadians));
+        for (Poisson autrespoissons : HelloApplication.banc) {
+            if (autrespoissons == this) {
+                continue;
+            } else if (vuePoisson.contains(autrespoissons.x, autrespoissons.y)) {
+                double dx = autrespoissons.x - x;
+                double dy = autrespoissons.y - y;
+                double distance = Math.sqrt(dx * dx + dy * dy);
+                double angleAbsolu = Math.toDegrees(Math.atan2(dy, dx));
+                double angleRelatif = angleAbsolu - direction;
+                if (distance < distanceVision/2) {
+                    if (angleRelatif < 0) {
+                        direction += 2;
+                    } else {
+                        direction -= 2;
+                    }
+                    vuePoisson.setFill(Color.RED);
+                } else {
+                    if (angleRelatif < 0.3) {
+                        direction -= 0.5;
+                    } else if (angleRelatif > 0.3) {
+                        direction += 0.5;
+                    }
+                    vuePoisson.setFill(Color.GREEN);
+                }
+            }
+
+        }
     }
 
 }
